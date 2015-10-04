a671f142b1964e4414072c42a6edccf30010269e
Samuel Gélineau <gelisam+github@gmail.com>
2015-10-04 14:08:51 -0400

    follow Pure.css's guidelines
    put the padding in a child div nested inside the parent div with the
    pure-u-* class, not on the parent div.
    
    I used view units ("vw", percent of viewport width, which is the window
    width) instead of percent for the new padding, otherwise the 5% of a
    pure-u-1-2 column did not match the 5% of a full-width pure-u-1.
    
    also, I've discovered that putting two pure-u-1-2 inside a pure-u-1 is
    messing things up, better to never nest pure-u classes and to instead
    repeat the pure-g as needed.
    
    a side effect of this change is that there is now more space between
    columns, because the padding is applied to each child column, not to the
    parent div. Still looks good, I think.

--- main.pl
+++ main.pl
@@ -150,74 +150,82 @@ app->start;
 __DATA__
 @@ meetup.html.ep
         <div class="pure-u-1">
-            <a href="<%= $url %>"><img class="meetup-logo" alt="meetup.com logo"
-                                       src="http://img2.meetupstatic.com/img/8308650022681532654/header/logo-2x.png">
-            <span class="date"><%= $date %></span><br>
-            <span class="addr"><%= $addr %></span><br></a>
+            <div class="padsides">
+                <a href="<%= $url %>"><img class="meetup-logo" alt="meetup.com logo"
+                                           src="http://img2.meetupstatic.com/img/8308650022681532654/header/logo-2x.png">
+                <span class="date"><%= $date %></span><br>
+                <span class="addr"><%= $addr %></span><br></a>
+             </div>
         </div>
 
 @@ talk.html.ep
         <!-- <%= $author_name %>'s talk -->
-        <div class="pure-u-lg-1-2 pure-u-1 talk-text">
-            <% if (defined stash->{talk_title}) { %>
-                <h3 class="title"><%= stash->{talk_title} %></h3>
-            <% } %>
+        <div class="pure-u-lg-1-2 pure-u-1">
+            <div class="padsides talk-text">
+                <% if (defined stash->{talk_title}) { %>
+                    <h3 class="title"><%= stash->{talk_title} %></h3>
+                <% } %>
 
-            <% if (defined stash->{talk_text}) { %>
-                <p><%== stash->{talk_text} %></p>
-            <% } %>
+                <% if (defined stash->{talk_text}) { %>
+                    <p><%== stash->{talk_text} %></p>
+                <% } %>
 
-            <% if (not defined stash->{talk_level}) { %>
-                <img alt="unknown level" src="https://img.shields.io/badge/talk%20level-unknown-lightgrey.svg"><br>
-            <% } elsif (stash->{talk_level} eq 'beginner') { %>
-                <img alt="beginner level" src="https://img.shields.io/badge/talk%20level-beginner-green.svg"><br>
-            <% } elsif (stash->{talk_level} eq 'intermediate') { %>
-                <img alt="intermediate level" src="https://img.shields.io/badge/talk%20level-intermediate-yellow.svg"><br>
-            <% } elsif (stash->{talk_level} eq 'expert') { %>
-                <img alt="expert level" src="https://img.shields.io/badge/talk%20level-expert-red.svg"><br>
-            <% } else { %>
-                <img alt="unknown level" src="https://img.shields.io/badge/talk%20level-unknown-lightgrey.svg"><br>
-            <% } %>
+                <% if (not defined stash->{talk_level}) { %>
+                    <img alt="unknown level" src="https://img.shields.io/badge/talk%20level-unknown-lightgrey.svg"><br>
+                <% } elsif (stash->{talk_level} eq 'beginner') { %>
+                    <img alt="beginner level" src="https://img.shields.io/badge/talk%20level-beginner-green.svg"><br>
+                <% } elsif (stash->{talk_level} eq 'intermediate') { %>
+                    <img alt="intermediate level" src="https://img.shields.io/badge/talk%20level-intermediate-yellow.svg"><br>
+                <% } elsif (stash->{talk_level} eq 'expert') { %>
+                    <img alt="expert level" src="https://img.shields.io/badge/talk%20level-expert-red.svg"><br>
+                <% } else { %>
+                    <img alt="unknown level" src="https://img.shields.io/badge/talk%20level-unknown-lightgrey.svg"><br>
+                <% } %>
 
-            <% if (defined stash->{talk_code}) { %>
-                %== badge_link_talk_code stash->{talk_code}
-            <% } %>
+                <% if (defined stash->{talk_code}) { %>
+                    %== badge_link_talk_code stash->{talk_code}
+                <% } %>
 
-            <% if (defined stash->{talk_slides}) { %>
-                %== badge_link_talk_slides stash->{talk_slides}
-            <% } %>
+                <% if (defined stash->{talk_slides}) { %>
+                    %== badge_link_talk_slides stash->{talk_slides}
+                <% } %>
+            </div>
         </div>
 
         <div class="pure-u-lg-1-4 pure-u-sm-1-2 pure-u-1">
-            <% if (defined stash->{author_avatar}) { %>
-                %== sprintf "<img alt=\"avatar\" class=\"avatar\" src=\"" . stash->{author_avatar} . "\">"
-            <% } else { %>
-                <div class="avatar" style="background-color: grey:"></div>
-            <% } %>
+            <div class="padsides">
+                <% if (defined stash->{author_avatar}) { %>
+                    %== sprintf "<img alt=\"avatar\" class=\"avatar\" src=\"" . stash->{author_avatar} . "\">"
+                <% } else { %>
+                    <div class="avatar" style="background-color: grey:"></div>
+                <% } %>
+            </div>
         </div>
 
         <div class="pure-u-lg-1-4 pure-u-sm-1-2 pure-u-1">
-            <p class="title"><%= $author_name %></p>
+            <div class="padsides">
+                <p class="title"><%= $author_name %></p>
 
-            <% if (defined stash->{author_github}) { %>
-                %== badge_link_author_github stash->{author_github}
-            <% } %>
+                <% if (defined stash->{author_github}) { %>
+                    %== badge_link_author_github stash->{author_github}
+                <% } %>
 
-            <% if (defined stash->{author_linkedin}) { %>
-                %== badge_link_author_linkedin stash->{author_linkedin}
-            <% } %>
+                <% if (defined stash->{author_linkedin}) { %>
+                    %== badge_link_author_linkedin stash->{author_linkedin}
+                <% } %>
 
-            <% if (defined stash->{author_reddit}) { %>
-                %== badge_link_author_reddit stash->{author_reddit}
-            <% } %>
+                <% if (defined stash->{author_reddit}) { %>
+                    %== badge_link_author_reddit stash->{author_reddit}
+                <% } %>
 
-            <% if (defined stash->{author_twitter}) { %>
-                %== badge_link_author_twitter stash->{author_twitter}
-            <% } %>
+                <% if (defined stash->{author_twitter}) { %>
+                    %== badge_link_author_twitter stash->{author_twitter}
+                <% } %>
 
-            <% if (defined stash->{author_website}) { %>
-                %== badge_link_author_website stash->{author_website}
-            <% } %>
+                <% if (defined stash->{author_website}) { %>
+                    %== badge_link_author_website stash->{author_website}
+                <% } %>
+            </div>
         </div>
 
 @@ index.html.ep
@@ -271,10 +279,18 @@ __DATA__
             margin: 5%;
         }
 
+        div.padbox {
+            padding: 3vw;
+        }
+        div.padbot {
+            padding: 0 3vw 3vw 3vw;
+        }
+        div.padsides {
+            padding: 0 3vw 0 3vw;
+        }
         div.purple-on-white {
             background-color: white;
             color: #342A52;
-            padding: 0 3% 3% 3%;
         }
 
         .purple-on-white a {
@@ -303,7 +319,6 @@ __DATA__
         div.white-on-purple {
             background-color: #342A52;
             color: white;
-            padding: 0 3% 3% 3%;
         }
 
         .white-on-purple a {
@@ -375,91 +390,109 @@ __DATA__
     </style>
 </head>
 
-<body><div class="pure-g">
-    <div class="white-on-purple">
+<body>
+    <div class="pure-g white-on-purple">
         <div class="pure-u-1">
-            <h1>Montr&eacute;al Haskellers Meetup</h1>
+            <div class="padsides">
+                <h1>Montr&eacute;al Haskellers Meetup</h1>
+            </div>
         </div>
 
         <div class="pure-u-md-1-2 pure-u-1">
-            <img src="https://wiki.haskell.org/wikiupload/4/4a/HaskellLogoStyPreview-1.png"
-                 class="haskell-logo" alt="Haskell Logo">
+            <div class="padsides">
+                <img src="https://wiki.haskell.org/wikiupload/4/4a/HaskellLogoStyPreview-1.png"
+                     class="haskell-logo" alt="Haskell Logo">
 
-            <p>We are the Montr&eacute;al Haskellers.</p>
+                <p>We are the Montr&eacute;al Haskellers.</p>
 
-            <p>We are a community that meets 6 times a year to discuss functional programming, welcome newcomers to the community and exchange amongst ourselves.</p>
+                <p>We are a community that meets 6 times a year to discuss functional programming, welcome newcomers to the community and exchange amongst ourselves.</p>
 
-            <p>We are aiming to expand our knowledge, understand our tools better and grow their usage.</p>
+                <p>We are aiming to expand our knowledge, understand our tools better and grow their usage.</p>
 
-            <p>You should be a part of that community.</p>
+                <p>You should be a part of that community.</p>
+            </div>
         </div>
 
         <div class="pure-u-md-1-2 pure-u-1">
-            <div class="twitter-widget"><a class="twitter-timeline" href="https://twitter.com/search?q=%23montreal%20%23haskell" data-widget-id="650461506920800256">Tweets about #montreal #haskell</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div>
+            <div class="padsides">
+                <div class="twitter-widget"><a class="twitter-timeline" href="https://twitter.com/search?q=%23montreal%20%23haskell" data-widget-id="650461506920800256">Tweets about #montreal #haskell</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div>
+            </div>
         </div>
 
         <div class="pure-u-1 links">
-            <a href="#next">Our Next Meetup</a> | <a href="#digs">Our Digs</a> | <a href="#previous">Our Previous Meetups</a> | <a href="#links">Links</a> | <a href="#sponsors">Sponsors</a>
+            <div class="padbot">
+                <a href="#next">Our Next Meetup</a> | <a href="#digs">Our Digs</a> | <a href="#previous">Our Previous Meetups</a> | <a href="#links">Links</a> | <a href="#sponsors">Sponsors</a>
+            </div>
         </div>
     </div>
 
-    <div class="purple-on-white">
+    <div class="pure-g purple-on-white">
         <div class="pure-u-1">
-            <h2><a id="next">Our Next Meetup</a></h2>
+            <div class="padsides">
+                <h2><a id="next">Our Next Meetup</a></h2>
+            </div>
         </div>
 
         <div class="pure-u-1">
-            <a href="http://www.meetup.com/Haskellers-Montreal-Meetup/events/223004218/"><img
-                  class="meetup-logo" alt="meetup.com logo"
-                  src="http://img2.meetupstatic.com/img/8308650022681532654/header/logo-2x.png">
+            <div class="padsides">
+                <a href="http://www.meetup.com/Haskellers-Montreal-Meetup/events/223004218/"><img
+                      class="meetup-logo" alt="meetup.com logo"
+                      src="http://img2.meetupstatic.com/img/8308650022681532654/header/logo-2x.png">
                 <span class="date">Wednesday, October 7, 2015 @ 18:30</span><br>
                 <span class="addr">RPM, 420 rue Guy, Montréal, QC</span></a>
+            </div>
         </div>
 
         <%== $next_meetup %>
 
     </div>
 
-    <div class="white-on-purple" style="width: 100%;">
+    <div class="pure-g white-on-purple" style="width: 100%;">
         <div class="pure-u-1">
+          <div class="padbot">
             <h2><a id="digs">Our digs: RPM, 420 Rue Guy, Montr&eacute;al</a></h2>
 
             <iframe style="width: 100%; border:0; min-height: 450px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2797.0165586740145!2d-73.56659279999997!3d45.48961130000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc91a638a6b442b%3A0xe315ad11de05f102!2sCentre+cloud.ca!5e0!3m2!1sen!2sca!4v1443915930301"></iframe>
+          </div>
         </div>
     </div>
 
-    <div class="purple-on-white">
+    <div class="pure-g purple-on-white">
         <div class="pure-u-1">
-            <h2><a id="previous">Our Previous Meetups</a></h2>
+            <div class="padsides">
+                <h2><a id="previous">Our Previous Meetups</a></h2>
+            </div>
         </div>
 
         <%== $previous_meetups %>
     </div>
 
-    <div class="white-on-purple">
-        <div style="padding: 3%" class="pure-u-1 pure-u-sm-1-2 white-on-purple">
-            <h2><a id="links">Links</a></h2>
+    <div class="pure-g white-on-purple">
+        <div class="pure-u-1 pure-u-sm-1-2 white-on-purple">
+            <div class="padbox">
+                <h2><a id="links">Links</a></h2>
 
-            <p>The <a href="http://www.haskell.org">Haskell Programming Language</a> homepage is the main resource to learn the language.</p>
+                <p>The <a href="http://www.haskell.org">Haskell Programming Language</a> homepage is the main resource to learn the language.</p>
 
-            <p>The <a href="https://github.com/christianlavoie/haskell-meetup">code for this website</a> is available for all to modify and improve. Send pull requests!</p>
+                <p>The <a href="https://github.com/christianlavoie/haskell-meetup">code for this website</a> is available for all to modify and improve. Send pull requests!</p>
 
-            <p><a href="http://www.meetup.com/Haskellers-Montreal-Meetup">Our Calendar</a> should provide you with a way to get reminders about our events and you can discuss Haskell topics on <a href="https://groups.google.com/forum/#!forum/haskellers-montreal">Our Google Group</a>.</p>
+                <p><a href="http://www.meetup.com/Haskellers-Montreal-Meetup">Our Calendar</a> should provide you with a way to get reminders about our events and you can discuss Haskell topics on <a href="https://groups.google.com/forum/#!forum/haskellers-montreal">Our Google Group</a>.</p>
 
-            <p>For those interested in other languages and technologies in Montr&eacute;al, the Montr&eacute;al Haskellers recommend the <a href="http://js-montreal.org/">JavaScript meetup</a>, the <a href="http://www.meetup.com/mtl-rb/">Ruby Meetup</a> and the <a href="http://big-data.meetup.com/cities/ca/qc/montr%C3%A9al/">BigData meetup</a>.</p>
+                <p>For those interested in other languages and technologies in Montr&eacute;al, the Montr&eacute;al Haskellers recommend the <a href="http://js-montreal.org/">JavaScript meetup</a>, the <a href="http://www.meetup.com/mtl-rb/">Ruby Meetup</a> and the <a href="http://big-data.meetup.com/cities/ca/qc/montr%C3%A9al/">BigData meetup</a>.</p>
+            </div>
         </div>
 
-        <div style="padding: 3%" class="pure-u-1 pure-u-sm-1-2 white-on-purple">
-            <h2><a id="sponsors">Sponsors</a></h2>
+        <div class="pure-u-1 pure-u-sm-1-2 white-on-purple">
+            <div class="padbox">
+                <h2><a id="sponsors">Sponsors</a></h2>
 
-            <p><strong><a href="http://www.sandreckoning.com">Sand Reckoning Consulting</a></strong> provides technical due diligence consulting for startup investors and infrastructure development for startups.</p>
+                <p><strong><a href="http://www.sandreckoning.com">Sand Reckoning Consulting</a></strong> provides technical due diligence consulting for startup investors and infrastructure development for startups.</p>
 
-            <p><strong><a href="https://cloud.ca">cloud.ca</a></strong>&apos;s platform works as a standalone IaaS and also fits perfectly into CloudOps&apos; hybrid cloud solutions, allowing a mix of private cloud and other public cloud infrastructures such as Amazon Web Services (AWS).</p>
+                <p><strong><a href="https://cloud.ca">cloud.ca</a></strong>&apos;s platform works as a standalone IaaS and also fits perfectly into CloudOps&apos; hybrid cloud solutions, allowing a mix of private cloud and other public cloud infrastructures such as Amazon Web Services (AWS).</p>
 
-            <!-- <p><strong><a href="http://d3center.ca/">District 3 Innovation Center</a></strong> is an entrepreneurial community within Concordia University at the heart of downtown Montréal, bringing people together to collaborate, create and innovate.</p> -->
+                <!-- <p><strong><a href="http://d3center.ca/">District 3 Innovation Center</a></strong> is an entrepreneurial community within Concordia University at the heart of downtown Montréal, bringing people together to collaborate, create and innovate.</p> -->
+            </div>
         </div>
     </div>
-</div>
-
 </body>
 </html>
